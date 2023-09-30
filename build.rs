768d87982f523744578d3014d4432023b991c61a
Isotr0py <2037008807@qq.com>
2023-10-01 00:43:56 +0800

    test static link build

---
+++ build.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("cargo:rustc-link-lib=static=jxl");
+    println!("cargo:rustc-link-lib=static=jxl_threads");
+
+    println!("cargo:rustc-link-lib=static=hwy");
+    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    println!("cargo:rustc-link-lib=static=brotlidec-static");
+    println!("cargo:rustc-link-lib=static=brotlienc-static");
+}
