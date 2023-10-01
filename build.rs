df5715b005983142e5ffb48cefee0c647a278cf4
Isotr0py <2037008807@qq.com>
2023-10-01 10:46:50 +0800

    add link stdc++

--- build.rs
+++ build.rs
@@ -6,4 +6,6 @@ fn main() {
     println!("cargo:rustc-link-lib=static=brotlicommon-static");
     println!("cargo:rustc-link-lib=static=brotlidec-static");
     println!("cargo:rustc-link-lib=static=brotlienc-static");
+
+    println!("cargo:rustc-link-lib=stdc++");
 }
