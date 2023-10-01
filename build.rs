61895ea7f9460f3d967a6d8d60e63a6b968835e2
Isotr0py <2037008807@qq.com>
2023-10-01 11:58:49 +0800

    fix brotli

--- build.rs
+++ build.rs
@@ -3,11 +3,9 @@ fn main() {
     println!("cargo:rustc-link-lib=static=jxl_threads");
 
     println!("cargo:rustc-link-lib=static=hwy");
-    println!("cargo:rustc-link-lib=static=brotli");
-    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
-    // println!("cargo:rustc-link-lib=static=brotlidec-static");
-    // println!("cargo:rustc-link-lib=static=brotlienc-static");
+    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    println!("cargo:rustc-link-lib=static=brotlidec-static");
+    println!("cargo:rustc-link-lib=static=brotlienc-static");
 
     println!("cargo:rustc-link-lib=stdc++");
-    // println!("cargo:rustc-link-lib=static=stdc++");
 }
