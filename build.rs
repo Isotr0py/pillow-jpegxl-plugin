df5e9eeecd671faa4f33966b4a9ddbe26ab331c0
Isotr0py <2037008807@qq.com>
2023-10-01 12:49:17 +0800

    test order

--- build.rs
+++ build.rs
@@ -3,9 +3,10 @@ fn main() {
     println!("cargo:rustc-link-lib=static=jxl_threads");
 
     println!("cargo:rustc-link-lib=static=hwy");
-    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
     println!("cargo:rustc-link-lib=static=brotlidec-static");
-    // println!("cargo:rustc-link-lib=static=brotlienc-static");
+    println!("cargo:rustc-link-lib=static=brotlienc-static");
+    println!("cargo:rustc-link-lib=static=brotlicommon-static");
 
     println!("cargo:rustc-link-lib=stdc++");
 }
