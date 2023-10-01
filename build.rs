4d64eff86075c8c473e35bb915051aa8dac5a77c
Isotr0py <2037008807@qq.com>
2023-10-01 20:48:21 +0800

    test container

--- build.rs
+++ build.rs
@@ -1,15 +1,14 @@
 fn main() {
     #[cfg(not(feature = "vendored"))]
     {
-        println!("cargo:rustc-link-lib=static=brotlidec-static");
         println!("cargo:rustc-link-lib=static=jxl");
         println!("cargo:rustc-link-lib=static=jxl_threads");
 
-        // println!("cargo:rustc-link-lib=hwy");
+        println!("cargo:rustc-link-lib=hwy");
 
-        // println!("cargo:rustc-link-lib=brotlidec");
-        // println!("cargo:rustc-link-lib=brotlienc");
-        // println!("cargo:rustc-link-lib=brotlicommon");
+        println!("cargo:rustc-link-lib=brotlidec");
+        println!("cargo:rustc-link-lib=brotlienc");
+        println!("cargo:rustc-link-lib=brotlicommon");
     }
 
     println!("cargo:rustc-link-lib=stdc++");
