6bc29d4953bc0b7533c36b25cb0784d416846629
Isotr0py <2037008807@qq.com>
2023-10-02 00:04:55 +0800

    fxxk brotlidec

--- build.rs
+++ build.rs
@@ -4,9 +4,12 @@ fn main() {
     #[cfg(not(feature = "vendored"))]
     {
         println!("cargo:rustc-link-lib=static=jxl");
+        println!("cargo:rustc-link-lib=static=brotlidec-static");
         println!("cargo:rustc-link-lib=static=jxl_threads");
+        println!("cargo:rustc-link-lib=static=brotlidec-static");
 
         println!("cargo:rustc-link-lib=static=hwy");
+        println!("cargo:rustc-link-lib=static=brotlidec-static");
         if let Ok(path) = env::var("DEP_HWY_LIB") {
             println!("cargo:rustc-link-search=native={}", path);
         }
@@ -17,9 +20,10 @@ fn main() {
         if let Ok(path) = env::var("DEP_BROTLI_LIB") {
             println!("cargo:rustc-link-search=native={}", path);
         }
+        println!("cargo:rustc-link-lib=static=brotlidec-static");
 
     }
 
     println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rerun-if-changed=build.rs");
+    // println!("cargo:rerun-if-changed=build.rs");
 }
