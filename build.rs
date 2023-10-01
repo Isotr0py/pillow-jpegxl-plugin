76687450fb5d17be2d21b85bc9808a3d54b477e6
Isotr0py <2037008807@qq.com>
2023-10-01 15:27:24 +0800

    add DEP_BROTLI_LIB

--- build.rs
+++ build.rs
@@ -1,13 +1,21 @@
+use std::env;
+
 fn main() {
-    // println!("cargo:rustc-link-lib=static=jxl");
-    // println!("cargo:rustc-link-lib=static=jxl_threads");
+    #[cfg(not(feature = "vendored"))]
+    {
+        println!("cargo:rustc-link-lib=static=jxl");
+        println!("cargo:rustc-link-lib=static=jxl_threads");
 
-    // println!("cargo:rustc-link-lib=static=hwy");
-    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
-    // println!("cargo:rustc-link-lib=static=brotlidec-static");
-    // println!("cargo:rustc-link-lib=static=brotlienc-static");
-    // println!("cargo:rustc-link-lib=static=brotlidec-static");
-    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
+        println!("cargo:rustc-link-lib=static=hwy");
 
-    // println!("cargo:rustc-link-lib=stdc++");
+        println!("cargo:rustc-link-lib=static=brotlicommon-static");
+        println!("cargo:rustc-link-lib=static=brotlidec-static");
+        println!("cargo:rustc-link-lib=static=brotlienc-static");
+        // println!("cargo:rustc-link-lib=static=brotlicommon-static");
+        if let Ok(path) = env::var("DEP_BROTLI_LIB") {
+            println!("cargo:rustc-link-search=native={}", path);
+        }
+    }
+
+    println!("cargo:rustc-link-lib=stdc++");
 }
