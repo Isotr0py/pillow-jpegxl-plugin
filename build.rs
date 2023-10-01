17a04796f132366a4c1b08b59e0b9aa29c24752b
Isotr0py <2037008807@qq.com>
2023-10-01 22:29:13 +0800

    remove brotlidec

--- build.rs
+++ build.rs
@@ -11,7 +11,7 @@ fn main() {
         //     println!("cargo:rustc-link-search=native={}", path);
         // }
 
-        println!("cargo:rustc-link-lib=static=brotlidec-static");
+        // println!("cargo:rustc-link-lib=static=brotlidec-static");
         // println!("cargo:rustc-link-lib=static=brotlienc-static");
         // println!("cargo:rustc-link-lib=static=brotlicommon-static");
         // if let Ok(path) = env::var("DEP_BROTLI_LIB") {
