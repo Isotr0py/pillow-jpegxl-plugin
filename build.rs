43d7986b74fd8137203ffcfc2338ac22a8fd88d5
Isotr0py <2037008807@qq.com>
2023-10-01 11:08:51 +0800

    remove jxlpy static link

--- build.rs
+++ build.rs
@@ -1,12 +1,12 @@
 fn main() {
-    println!("cargo:rustc-link-lib=static=jxl");
-    println!("cargo:rustc-link-lib=static=jxl_threads");
+    // println!("cargo:rustc-link-lib=static=jxl");
+    // println!("cargo:rustc-link-lib=static=jxl_threads");
 
-    println!("cargo:rustc-link-lib=static=hwy");
-    println!("cargo:rustc-link-lib=static=brotlicommon-static");
-    println!("cargo:rustc-link-lib=static=brotlidec-static");
-    println!("cargo:rustc-link-lib=static=brotlienc-static");
+    // println!("cargo:rustc-link-lib=static=hwy");
+    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    // println!("cargo:rustc-link-lib=static=brotlidec-static");
+    // println!("cargo:rustc-link-lib=static=brotlienc-static");
 
-    // println!("cargo:rustc-link-lib=stdc++");
-    println!("cargo:rustc-link-lib=static=stdc++");
+    println!("cargo:rustc-link-lib=stdc++");
+    // println!("cargo:rustc-link-lib=static=stdc++");
 }
