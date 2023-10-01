a045a90e4d28d4bbc7a92e8b187a96a1b858ff57
Isotr0py <2037008807@qq.com>
2023-10-01 19:56:27 +0800

    fix build order

--- build.rs
+++ build.rs
@@ -7,8 +7,8 @@ fn main() {
 
         println!("cargo:rustc-link-lib=static=hwy");
 
-        println!("cargo:rustc-link-lib=static=jxl");
         println!("cargo:rustc-link-lib=static=jxl_threads");
+        println!("cargo:rustc-link-lib=static=jxl");
     }
 
     println!("cargo:rustc-link-lib=stdc++");
