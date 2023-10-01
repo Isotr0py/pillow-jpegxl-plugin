b0b7a855c691b235d45102c0ba5cc8219d464866
Isotr0py <2037008807@qq.com>
2023-10-01 13:39:30 +0800

    remove brotli

--- build.rs
+++ build.rs
@@ -6,8 +6,8 @@ fn main() {
     // println!("cargo:rustc-link-lib=static=brotlicommon-static");
     // println!("cargo:rustc-link-lib=static=brotlidec-static");
     // println!("cargo:rustc-link-lib=static=brotlienc-static");
-    println!("cargo:rustc-link-lib=static=brotlidec-static");
-    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    // println!("cargo:rustc-link-lib=static=brotlidec-static");
+    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
 
     println!("cargo:rustc-link-lib=stdc++");
 }
