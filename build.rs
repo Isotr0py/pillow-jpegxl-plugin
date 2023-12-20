38158ae2f8ea1c08b5d4e0712e880b3232ba6688
Isotr0py <2037008807@qq.com>
2023-12-20 14:52:55 +0800

    add dynamic link

--- build.rs
+++ build.rs
@@ -1,24 +1,46 @@
 use std::env;
 
+fn dynamic_link() {
+    println!("cargo:rustc-link-lib=jxl");
+    println!("cargo:rustc-link-lib=jxl_threads");
+
+    println!("cargo:rustc-link-lib=hwy");
+    if let Ok(path) = env::var("DEP_HWY_LIB") {
+        println!("cargo:rustc-link-search=native={}", path);
+    }
+
+    println!("cargo:rustc-link-lib:+whole-archive=brotlidec");
+    println!("cargo:rustc-link-lib=brotlienc");
+    println!("cargo:rustc-link-lib=brotlicommon");
+    if let Ok(path) = env::var("DEP_BROTLI_LIB") {
+        println!("cargo:rustc-link-search=native={}", path);
+    }
+}
+
+fn static_link() {
+    println!("cargo:rustc-link-lib=static=jxl");
+    println!("cargo:rustc-link-lib=static=jxl_threads");
+
+    println!("cargo:rustc-link-lib=static=hwy");
+    if let Ok(path) = env::var("DEP_HWY_LIB") {
+        println!("cargo:rustc-link-search=native={}", path);
+    }
+
+    println!("cargo:rustc-link-lib=static:+whole-archive=brotlidec-static");
+    println!("cargo:rustc-link-lib=static=brotlienc-static");
+    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    if let Ok(path) = env::var("DEP_BROTLI_LIB") {
+        println!("cargo:rustc-link-search=native={}", path);
+    }
+}
+
 fn main() {
     // Static link libjxl
-    #[cfg(not(feature = "vendored"))]
-    {
-        println!("cargo:rustc-link-lib=static=jxl");
-        println!("cargo:rustc-link-lib=static=jxl_threads");
+    #[cfg(all(not(feature = "vendored"), not(feature = "dynamic")))]
+    static_link();
 
-        println!("cargo:rustc-link-lib=static=hwy");
-        if let Ok(path) = env::var("DEP_HWY_LIB") {
-            println!("cargo:rustc-link-search=native={}", path);
-        }
-
-        println!("cargo:rustc-link-lib=static:+whole-archive=brotlidec-static");
-        println!("cargo:rustc-link-lib=static=brotlienc-static");
-        println!("cargo:rustc-link-lib=static=brotlicommon-static");
-        if let Ok(path) = env::var("DEP_BROTLI_LIB") {
-            println!("cargo:rustc-link-search=native={}", path);
-        }
-    }
+    #[cfg(all(not(feature = "vendored"), feature = "dynamic"))]
+    dynamic_link();
 
     // Dynamic link c++
     // TODO: Support MSVC and use Cargo
@@ -28,6 +50,6 @@ fn main() {
         // Linux and Windows should all use GNU toolchain
         "linux" | "windows" => println!("cargo:rustc-link-lib=stdc++"),
         "macos" => println!("cargo:rustc-link-lib=c++"),
-        _ => panic!("Not implemented c++ link on {}", platform)
+        _ => panic!("Not implemented c++ link on {}", platform),
     }
 }
