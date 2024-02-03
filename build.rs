97d4984c2ccd559f538dea6c09aaf852c03e1d1d
Isotr0py <41363108+Isotr0py@users.noreply.github.com>
2024-02-03 22:56:17 +0800

    ⬆️: Upgrade jpegxl-rs → v0.9.0, pyo3 → v0.20.2 (#16)
    * ⬆️: Upgrade libjxl 0.9.1
    
    * use dynamic link
    
    * upgrade pyo3
    
    * fix static link
    
    * use static link

--- build.rs
+++ build.rs
@@ -26,9 +26,12 @@ fn static_link() {
         println!("cargo:rustc-link-search=native={}", path);
     }
 
-    println!("cargo:rustc-link-lib=static:+whole-archive=brotlidec-static");
-    println!("cargo:rustc-link-lib=static=brotlienc-static");
-    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    // println!("cargo:rustc-link-lib=static:+whole-archive=brotlidec-static");
+    // println!("cargo:rustc-link-lib=static=brotlienc-static");
+    // println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    println!("cargo:rustc-link-lib:+whole-archive=brotlidec");
+    println!("cargo:rustc-link-lib=brotlienc");
+    println!("cargo:rustc-link-lib=brotlicommon");
     if let Ok(path) = env::var("DEP_BROTLI_LIB") {
         println!("cargo:rustc-link-search=native={}", path);
     }
