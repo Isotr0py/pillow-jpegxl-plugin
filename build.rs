778e9116be0a005b24d002692e2a8ee08574b904
Isotr0py <2037008807@qq.com>
2024-02-04 17:54:36 +0800

    :bug: (CI): fix brotli static link

--- build.rs
+++ build.rs
@@ -26,9 +26,9 @@ fn static_link() {
         println!("cargo:rustc-link-search=native={}", path);
     }
 
-    println!("cargo:rustc-link-lib=static:+whole-archive=brotlidec-static");
-    println!("cargo:rustc-link-lib=static=brotlienc-static");
-    println!("cargo:rustc-link-lib=static=brotlicommon-static");
+    println!("cargo:rustc-link-lib=static:+whole-archive=brotlidec");
+    println!("cargo:rustc-link-lib=static=brotlienc");
+    println!("cargo:rustc-link-lib=static=brotlicommon");
     if let Ok(path) = env::var("DEP_BROTLI_LIB") {
         println!("cargo:rustc-link-search=native={}", path);
     }
