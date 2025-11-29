3427fff9dfc617cbaa79cd857f2175f3f21d2405
isotr0py <2037008807@qq.com>
2025-11-29 17:03:31 +0800

    code format
    Signed-off-by: isotr0py <2037008807@qq.com>

--- test/images/create_f16_image.py
+++ test/images/create_f16_image.py
@@ -1,12 +1,11 @@
-import OpenEXR
 import numpy as np
+import OpenEXR
 
 height, width = (50, 40)
-img_f16 = np.random.rand(height, width, 3).astype('float16')
+img_f16 = np.random.rand(height, width, 3).astype("float16")
 
-channels = { "RGB" : img_f16 }
-header = { "compression" : OpenEXR.ZIP_COMPRESSION,
-           "type" : OpenEXR.scanlineimage }
+channels = {"RGB": img_f16}
+header = {"compression": OpenEXR.ZIP_COMPRESSION, "type": OpenEXR.scanlineimage}
 
 with OpenEXR.File(header, channels) as outfile:
     outfile.write("random_image_f16.exr")
